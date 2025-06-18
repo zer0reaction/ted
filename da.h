1048836e9d7d3dbb4199435f327a9d5ce65e2d84
zer0reaction <boltgopnik@gmail.com>
2025-06-18 10:54:35 +0300

    Added initializing func and assertions in da.h

--- da.h
+++ da.h
@@ -1,18 +1,20 @@
 /*
- * Dynamic array header - last edited by zer0 on 15 Jun 2025
+ * Dynamic array header - last edited by zer0 on 18 Jun 2025
  * Written in C99.
  * 
  * Usage:
  * 
  * 1.  DA_TYPEDEF(type, name) to define a struct with the specified name
  * 2.  name_create() to allocate memory and return struct by value
+ * 3.  Optionally initialize the memory by calling name_init_with_zeros(name *)
  * ... Do whatever
  * n.  name_destroy(type *) to free memory
  *
- * All allocated memory is not initialized.
+ * All allocated memory is not initialized by default.
  * 
  * DA_INIT_CAP sets the initial capacity of the array. Capacity can't go below
- * it by using generated functions (is this the right way to do this?).
+ * it by using generated functions (is this the right way to do this?). Only
+ * name_destroy(name *) sets the capacity to 0.
  *
  * malloc and realloc were used, so calculated values are not protected from
  * overflow (you shouldn't allocate that much memory anyway).
@@ -24,6 +26,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #define DA_INIT_CAP 128
 
@@ -42,6 +45,7 @@ static name name##_create(void)             \
                                             \
     a.cap = DA_INIT_CAP;                    \
     a.data = malloc(sizeof(T) * a.cap);     \
+    assert(a.data && "Buy more RAM");       \
     a.size = 0;                             \
                                             \
     return a;                               \
@@ -54,11 +58,17 @@ static void name##_destroy(name *a) \
     a->size = a->cap = 0;           \
 }                                   \
 \
+static void name##_init_with_zeros(name *a) \
+{                                           \
+    memset(a->data, 0, sizeof(T) * a->cap); \
+}                                           \
+\
 static void name##_shrink_to_fit(name *a)               \
 {                                                       \
     if (a->size <= a->cap / 4) {                        \
         a->cap = MAX(DA_INIT_CAP, a->size * 2);         \
         a->data = realloc(a->data, sizeof(T) * a->cap); \
+        assert(a->data && "Buy more RAM");              \
     }                                                   \
 }                                                       \
 \
@@ -73,6 +83,7 @@ static void name##_clear(name *a)                   \
     a->size = 0;                                    \
     a->cap = DA_INIT_CAP;                           \
     a->data = realloc(a->data, sizeof(T) * a->cap); \
+    assert(a->data && "Buy more RAM");              \
 }                                                   \
 \
 static void name##_push_back(name *a, T item)           \
@@ -84,6 +95,7 @@ static void name##_push_back(name *a, T item)           \
     }                                                   \
     if (needs_realloc) {                                \
         a->data = realloc(a->data, sizeof(T) * a->cap); \
+        assert(a->data && "Buy more RAM");              \
     }                                                   \
                                                         \
     a->data[a->size] = item;                            \
@@ -99,6 +111,7 @@ static void name##_push_back_many(name *a, const T* items, size_t n)    \
     }                                                                   \
     if (needs_realloc) {                                                \
         a->data = realloc(a->data, sizeof(T) * a->cap);                 \
+        assert(a->data && "Buy more RAM");                              \
     }                                                                   \
                                                                         \
     memcpy(&a->data[a->size], items, sizeof(T) * n);                    \
@@ -115,6 +128,7 @@ static T name##_pop_back(name *a)                           \
     if (a->size <= a->cap / 4) {                            \
         a->cap = MAX(DA_INIT_CAP, a->size * 2);             \
         a->data = realloc(a->data, sizeof(T) * a->cap);     \
+        assert(a->data && "Buy more RAM");                  \
     }                                                       \
                                                             \
     return item;                                            \
@@ -134,6 +148,7 @@ static void name##_push_many(name    *a,                                    \
     }                                                                       \
     if (needs_realloc) {                                                    \
         a->data = realloc(a->data, sizeof(T) * a->cap);                     \
+        assert(a->data && "Buy more RAM");                                  \
     }                                                                       \
                                                                             \
     memmove(&a->data[pos + n], &a->data[pos], sizeof(T) * (a->size - pos)); \
@@ -154,6 +169,7 @@ static void name##_delete_many(name *a, size_t pos, size_t n)       \
     if (a->size <= a->cap / 4) {                                    \
         a->cap = MAX(DA_INIT_CAP, a->size * 2);                     \
         a->data = realloc(a->data, sizeof(T) * a->cap);             \
+        assert(a->data && "Buy more RAM");                          \
     }                                                               \
 }
 
